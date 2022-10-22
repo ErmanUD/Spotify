47f5ef422d20dac2efa5459425bb7a02f152cdc7
Erman Ufuk Demirci <ermanufukdemirci@gmail.com>
2022-10-22 18:05:49 +0300

    Create Playlist - ViewController

---
+++ Spotify/ViewModels/PlaylistHeaderViewViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  PlaylistHeaderViewViewModel.swift
+//  Spotify
+//
+//  Created by Erman Ufuk Demirci on 22.10.2022.
+//
+
+import Foundation
+
+struct PlaylistHeaderViewViewModel {
+    let name: String
+    let ownerName: String?
+    let description: String?
+    let artworkURL: URL?
+    
+}
---
+++ Spotify/Views/PlaylistHeaderCollectionReusableView.swift
@@ -0,0 +1,145 @@
+//
+//  PlaylistHeaderCollectionReusableView.swift
+//  Spotify
+//
+//  Created by Erman Ufuk Demirci on 22.10.2022.
+//
+
+import UIKit
+
+final class PlaylistHeaderCollectionReusableView: UICollectionReusableView {
+    static let identifier = "PlaylistHeaderCollectionReusableView"
+    
+    weak var delegate: PlaylistHeaderCollectionReusableViewDelegate?
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 22, weight: .semibold)
+        
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    private let ownerLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .secondaryLabel
+        label.font = .systemFont(ofSize: 18, weight: .light)
+        
+        return label
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(systemName: "photo")
+        
+        return imageView
+    }()
+    
+    private let playAllButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemGreen
+        let image = UIImage(systemName: "play.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 44, weight: UIImage.SymbolWeight.regular))
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.layer.cornerRadius = 27
+        button.layer.masksToBounds = true
+        
+        return button
+    }()
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .systemBackground
+        addSubview(imageView)
+        addSubview(nameLabel)
+        addSubview(descriptionLabel)
+        addSubview(ownerLabel)
+        addSubview(playAllButton)
+        playAllButton.addTarget(self, action: #selector(didTapPlayAll), for: .touchUpInside)
+    }
+    
+    @objc private func didTapPlayAll() {
+        delegate?.playlistHeaderCollectionReusableViewDidTapPlayAll(self)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let imageSize: CGFloat = height/1.8
+        imageView.frame = CGRect(
+            x: (width - imageSize)/2,
+            y: 20,
+            width: imageSize,
+            height: imageSize
+        )
+        
+        nameLabel.frame = CGRect(
+            x: 10,
+            y: imageView.bottom,
+            width: width - 20,
+            height: 44
+        )
+        
+        descriptionLabel.frame = CGRect(
+            x: 10,
+            y: nameLabel.bottom,
+            width: width - 20,
+            height: 44
+        )
+        
+        ownerLabel.frame = CGRect(
+            x: 10,
+            y: descriptionLabel .bottom,
+            width: width - 20,
+            height: 44
+        )
+        
+        playAllButton.frame = CGRect(
+            x: width - 80,
+            y: height - 100,
+            width: 70,
+            height: 70
+        )
+    }
+    
+    func configure(with viewModel: PlaylistHeaderViewViewModel) {
+        nameLabel.text = viewModel.name
+        ownerLabel.text = viewModel.ownerName
+        descriptionLabel.text = viewModel.description
+        imageView.image = loadImage(url: viewModel.artworkURL)
+    }
+    
+    func loadImage(url: URL?) -> UIImage {
+        guard let url = url else {
+            return UIImage(systemName: "photo")!
+        }
+
+        if let data = try? Data(contentsOf: url) {
+            if let image = UIImage(data: data) {
+                
+                return image
+            }
+        }
+        
+        return UIImage(systemName: "photo")!
+    }
+}
+
+protocol PlaylistHeaderCollectionReusableViewDelegate: AnyObject {
+    func playlistHeaderCollectionReusableViewDidTapPlayAll(_ header: PlaylistHeaderCollectionReusableView)
+}
